5a545ad1635d662ea3e07af067f509b34747c6c8
Chanadu <chandu.peddada@gmail.com>
2024-06-04 08:50:22 -0500

    removing pocketbase

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,16 +1,14 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 
-import PocketBase from 'pocketbase';
 declare global {
 	namespace App {
 		// interface Error {}
-		interface Locals {
-			pb: PocketBase;
-			id: string;
-			email: string;
-		}
-
+		//		interface Locals {
+		//			pb: PocketBase;
+		//			id: string;
+		//			email: string;
+		//		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
