309afa54a35fbb98169be4263f1f73b4e42d061a
Chanadu <chandu.peddada@gmail.com>
2024-06-06 12:40:25 -0500

    vercel

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { inject } from '@vercel/analytics';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import { onMount } from 'svelte';
 	import '../app.css';
 	import Navbar from './navbar.svelte';
@@ -18,6 +20,8 @@
 			:	document.documentElement.classList.remove('dark');
 		}
 	}
+	injectSpeedInsights();
+	inject();
 </script>
 
 <div class="m-0 min-h-screen bg-background text-text transition-all duration-100">
