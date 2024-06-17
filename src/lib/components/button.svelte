3884fc0408cd28bcb4fbd55a550f7dc19cf5ce78
Chanadu <chandu.peddada@gmail.com>
2024-06-17 12:02:05 -0500

    fix home page

--- src/lib/components/button.svelte
+++ src/lib/components/button.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	export let onClick: () => void;
-	export let extraClasses: string = 'p-4';
+	export let extraClasses: string = 'p-4 w-full h-full';
 </script>
 
 <button
-	class="{extraClasses} diabled:pointer-events-none relative flex h-full w-full cursor-pointer select-none items-center justify-center rounded-2xl border-2 border-primary bg-container text-center align-middle shadow-xl transition-all duration-200 hover:-translate-y-2 hover:scale-105 hover:border-accent active:translate-y-0 active:scale-95 active:shadow-none"
+	class="{extraClasses} diabled:pointer-events-none relative flex cursor-pointer select-none items-center justify-center rounded-2xl border-2 border-primary bg-container text-center align-middle shadow-xl transition-all duration-200 hover:-translate-y-2 hover:scale-105 hover:border-accent active:translate-y-0 active:scale-95 active:shadow-none"
 	on:click="{onClick}"
 >
 	<slot></slot>
